0bbe58682698ca222b679323ed6fc715b9ca8a82
Srdjan S <stankovic.srdjo@gmail.com>
2020-09-15 21:33:37 +0200

    Update example and README.md

--- examples/app.py
+++ examples/app.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
 
     import uvicorn
 
-    uvicorn.run("examples:app", host='0.0.0.0', port=8000, reload=True, debug=False)
+    uvicorn.run("examples.app:app", host='0.0.0.0', port=8000, reload=True, debug=False)
